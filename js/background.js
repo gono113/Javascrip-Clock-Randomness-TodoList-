5c077fb62bb5c7483bfd4671726044d48343886e
gono113 <gono4560@gmail.com>
2025-06-24 16:02:31 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -13,4 +13,5 @@ const bgImage = document.createElement('img');
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
-// appenChild = createElement로 생성한 요소를 자식으로 생성해줌(위에선 body의 자식으로)
+// appenChild = (가장 아래에 오도록) createElement로 생성한 요소를 자식으로 생성해줌(위에선 body의 자식으로)
+// prepand도 가능 (가장 위에 오도록)
