3a5d4076b0d955bf16182a380bfa538f8c952e86
gono113 <gono4560@gmail.com>
2025-06-24 15:05:01 +0900

    quotes_js
    made quotes.js file

---
+++ js/quotes.js
@@ -0,0 +1,42 @@
+const quotes = [
+    {
+      quote: "The best way to get started is to quit talking and begin doing.",
+      author: "Walt Disney"
+    },
+    {
+      quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+      author: "Winston Churchill"
+    },
+    {
+      quote: "Don't watch the clock; do what it does. Keep going.",
+      author: "Sam Levenson"
+    },
+    {
+      quote: "It always seems impossible until it’s done.",
+      author: "Nelson Mandela"
+    },
+    {
+      quote: "The future belongs to those who believe in the beauty of their dreams.",
+      author: "Eleanor Roosevelt"
+    },
+    {
+      quote: "You miss 100% of the shots you don’t take.",
+      author: "Wayne Gretzky"
+    },
+    {
+      quote: "Believe you can and you're halfway there.",
+      author: "Theodore Roosevelt"
+    },
+    {
+      quote: "Hardships often prepare ordinary people for an extraordinary destiny.",
+      author: "C.S. Lewis"
+    },
+    {
+      quote: "I have not failed. I've just found 10,000 ways that won't work.",
+      author: "Thomas A. Edison"
+    },
+    {
+      quote: "Do what you can, with what you have, where you are.",
+      author: "Theodore Roosevelt"
+    }
+  ];
