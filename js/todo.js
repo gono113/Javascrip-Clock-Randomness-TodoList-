45170b3533784899831a3600fc007c8454d79087
gono113 <gono4560@gmail.com>
2025-06-24 23:03:14 +0900

    JSON,  localStorage
    how to change array to object
    and keep the objects on a screen

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,9 @@ const toDoForm = document.querySelector('#todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('#todo-list');
 
-const toDos = [];
+const TODOS_KEY = 'todos';
+
+let toDos = [];
 
 function saveToDos() {
     localStorage.setItem('todos', JSON.stringify(toDos));
@@ -34,3 +36,22 @@ function handleToDoSubmit(event) {
     saveToDos();
 }
 toDoForm.addEventListener('submit', handleToDoSubmit);
+
+// function sayHello(item) {
+//     console.log(`this is the turn off ${item}`);
+// }
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if(savedToDos){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+/**
+ * 어레이가 빈값으로 시작하기 떄문에
+ * 새로고침시 다시 비어있는 어레이로 돌아감
+ * (let)toDos = parsedToDo를 정의함으로써
+ * 기존에 어레이를 저장하고 새로 덮어씌워지지 않고
+ * 기존에 저장되있던 항목을 그대로 가져와서 함수 실행
+ */
